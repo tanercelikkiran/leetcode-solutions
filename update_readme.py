ebf3efec70e6956ae848a2629daad22d4f5943e5
Taner Çelikkıran <tanercelikkiran@gmail.com>
2023-04-10 17:14:57 +0300

    Add script to update solved question number

--- update_readme.py
+++ update_readme.py
@@ -1,15 +1,23 @@
-import requests
-import json
+import os
 
-url = 'https://leetcode.com/api/problems/all/'
-response = requests.get(url)
-num_solved = json.loads(response.content.decode('utf-8'))['num_solved']
-num_total = json.loads(response.content.decode('utf-8'))['num_total']
+folder_path = "solutions"
 
-# Find the line that contains the number of solved problems and replace it
+file_count = 0
 
-with open('README.md', 'r') as f:
+file_count += len([name for name in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, name))])
+
+#Read the README.md file and change the number of problems solved by finding the line with the number of problems solved
+
+with open("README.md", "r") as f:
+    f.seek(0)
     lines = f.readlines()
     for i, line in enumerate(lines):
-        if 'solved' in line:
-            lines[i] = 'I solved {} problem out of {} so far.'.format(num_solved, num_total)
+        if "solved" in line:
+            lines[i] = "I solved **" + str(file_count) + "** problems so far.\n"
+            break
+
+#Write the new README.md file
+
+with open("README.md", "w") as f:
+    f.writelines(lines)
+
