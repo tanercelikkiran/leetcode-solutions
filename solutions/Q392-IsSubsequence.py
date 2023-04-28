d58c32e7a7155c15b84313a91e632aefd1c8ce75
Taner Çelikkıran <tanercelikkiran@gmail.com>
2023-04-29 02:23:17 +0300

    Create Q392-IsSubsequence.py

---
+++ solutions/Q392-IsSubsequence.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        dp = [[0] * (len(t) + 1) for _ in range(len(s) + 1)]
+
+        for i in range(1, len(s) + 1):
+            for j in range(1, len(t) + 1):
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+                if s[i-1] == t[j-1]:
+                    dp[i][j] = max(dp[i][j], dp[i-1][j-1] + 1)
+
+        return dp[-1][-1] == len(s)
