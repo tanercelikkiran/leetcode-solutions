13cb097a6fd40bf71cfedd6e9cd70cb039d1c717
Taner Çelikkıran <tanercelikkiran@gmail.com>
2023-05-06 17:07:54 +0300

    Create Q1822-SignOfTheProductOfTheArray.py

---
+++ solutions/Q1822-SignOfTheProductOfTheArray.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+    def arraySign(self, nums: List[int]) -> int:
+        if 0 in nums:
+            return 0
+        count = 0
+        for num in nums:
+            if num < 0:
+                count += 1
+        return -1 if count % 2 == 1 else 1
