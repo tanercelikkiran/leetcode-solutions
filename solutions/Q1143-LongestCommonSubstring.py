4f73f27f1410faeb36b4ec00903f069dc061479b
Taner Çelikkıran <tanercelikkiran@gmail.com>
2023-06-04 18:59:04 +0300

    Update Q1143-LongestCommonSubstring.py

--- solutions/Q1143-LongestCommonSubstring.py
+++ solutions/Q1143-LongestCommonSubstring.py
@@ -4,7 +4,7 @@ class Solution:
 
         for i in range(1, len(text1)+1):
             for j in range(1, len(text2)+1):
-                if text1[i] == text2[j]:
+                if (text1[i-1] == text2[j-1]):
                     dp[i][j] = dp[i-1][j-1]+1
                 else:
                     dp[i][j] = max(dp[i-1][j], dp[i][j-1])
